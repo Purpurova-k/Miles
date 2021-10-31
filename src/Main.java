1cc5f79cc3d39c10334bdb1a1ad6f5ac3857cc62
Purpurova-k <katrin.purpurova@gmail.com>
2021-10-31 14:48:19 +0300

    IDEA project added

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 1000;
+        // Здесь меняем стоимость билета
+        int mile = 20;
+        int total = ticket / mile;
+        System.out.println("Количество бонусных миль: " + total);
+    }
+
+
+}
